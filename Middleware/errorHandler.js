81bedb079cf10d59f45839b7bca4493331aa328c
Samnit619 <samnit.s472@gmail.com>
2024-05-22 23:01:00 -0400

    added constants and modified error handling

--- Middleware/errorHandler.js
+++ Middleware/errorHandler.js
@@ -1,6 +1,51 @@
-const errorHandler = (err, req, res, next ) => {
-    const statusCode = res.statusCode ? res.statusCode: 500; 
-    res.json({ message: err.message, stackTrace: err.stack});
+const {constants} = require("../constants");
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Not found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "Server error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+        console.log("No Error, good");
+      break;
+  }
 };
 
 module.exports = errorHandler;
