e7f53face21ec026f50595b6f9b509ec2ead81e4
Samnit619 <samnit.s472@gmail.com>
2024-06-11 16:52:40 -0400

    deleted frontend

--- frontend/contact-manager/src/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-export const NavBar = () => {
-    return(
-       <div className=" flex justify-between w-[1440px] h-[100px] bg-slate-500 p-5">
-        <div className=" text-slate-700">Hello World</div>
-        <div className="">buttons</div>
-
-       </div>
-    )
-}
