ee27ed07a08816a8e6743ead716150b7bfcef72b
Samnit619 <samnit.s472@gmail.com>
2024-06-27 20:24:16 -0400

    added shadcn comps and darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import NavBar from "./components/NavBar";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-  <div className="">
-      <NavBar/>
-  </div>
-
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <NavBar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
