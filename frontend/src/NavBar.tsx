29ff92e42594a5bffc93d4a2b277e0bf66c25c98
Samnit619 <samnit.s472@gmail.com>
2024-06-28 16:28:12 -0400

    added logo and created NavBar

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -1,19 +1,22 @@
+import { ModeToggle } from "./components/ui/mode-toggle";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center px-5 py-5">
-        <div className=" w-36">Logo</div>
-        <div className=" flex gap-4">
-            <div>Contacts</div>
-            <div>Favorites</div>
-            <div>Tags</div>
-        </div>
-        <div className=" flex gap-4">
-            <div>Settings</div>
-            <img src="" alt="No Img" />
-        </div>
+    <div className="md:w-[300px] border-r">
+<div className="flex gap-5">
+    <div className="flex items-center md:px-5 md:py-7 md:w-[250px] gap-2 ">
+      <div className=" w-[35px] "><img src="../../public/logo/logo1.png" alt=""  /></div>
+      <div className=" ubuntu-bold text-xl">ContactHub</div>
     </div>
-  )
-}
+    <div className="md:py-7 md:px-4">
+      <ModeToggle/>
+    </div>
+
+</div>
+
+    </div>
+
+  );
+};
 
 export default NavBar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        primaryBlue:"#",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
