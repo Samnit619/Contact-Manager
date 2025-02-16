ff1c8bbdbe570dc83ccf95b975987611deeb47cb
Samnit619 <samnit.s472@gmail.com>
2025-02-15 22:47:24 -0500

    email not required when adding contact

--- backend/Models/contactModel.js
+++ backend/Models/contactModel.js
@@ -2,44 +2,41 @@ const mongoose = require("mongoose");
 
 const ContactSchema = mongoose.Schema(
   {
-    
-      user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-      },
-      name: {
-        type: String,
-        required: [true, "Please add the contact name"],
-      },
-      email: {
-        type: String,
-        required: [true, "Please add the contact mail address"],
-      },
-      phone: {
-        type: String,
-        required: [true, "Please add the contact phone number"],
-      },
-      fav: {
-        type: Boolean,
-      },
-      relation:{
-        type:String,
-      },
-      birthday:{
-        type:String,
-      },
-      tags:{
-        type:[String],
-      },
-      description:{
-        type:String,
-      }
-    
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: [true, "Please add the contact name"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: [true, "Please add the contact phone number"],
+    },
+    fav: {
+      type: Boolean,
+    },
+    relation: {
+      type: String,
+    },
+    birthday: {
+      type: String,
+    },
+    tags: {
+      type: [String],
+    },
+    description: {
+      type: String,
+    },
   },
-    {
-      timestamps: true,
-    }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Contact", ContactSchema);
